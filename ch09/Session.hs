51f80e9a91b76f1a56b15329fd40ecc552ba0b63
Sébastien Canart <sebastien.canart@onprvp.fgov.be>
2014-09-23 14:19:17 +0200

    Display message

--- ch09/Session.hs
+++ ch09/Session.hs
@@ -7,12 +7,28 @@ data App = App
 
 mkYesod "App" [parseRoutes|
 / HomeR GET POST
+/set-message SetMessageR POST
 |]
 
 instance Yesod App where
     makeSessionBackend _ = do
         backend <- defaultClientSessionBackend 1 "keyfile.aes"
         return $ Just backend
+    defaultLayout widget = do
+        pc <- widgetToPageContent widget
+        mmsg <- getMessage
+        withUrlRenderer [hamlet|
+$doctype 5
+<html>
+    <head>
+        <title>#{pageTitle pc}
+        ^{pageHead pc}
+    <body>
+        $maybe msg <- mmsg
+            <p>Your message was: #{msg}
+        ^{pageBody pc}
+|]
+
 
 instance RenderMessage App FormMessage where
     renderMessage _ _ = defaultFormMessage
@@ -21,11 +37,16 @@ getHomeR :: Handler Html
 getHomeR = do
     sess <- getSession
     defaultLayout [whamlet|
+    <h1>#{show sess}
     <form method=post>
         <input type=text name=key>
         <input type=text name=val>
         <input type=submit>
-    <h1>#{show sess}
+    <hr>
+    <form method=post action=@{SetMessageR}>
+        My message is: #
+        <input type=text name=message>
+        <button>Go
 |]
 
 postHomeR :: Handler ()
@@ -35,6 +56,12 @@ postHomeR = do
         Nothing -> deleteSession key
         Just val -> setSession key val
     liftIO $ print (key, mval)
+    redirect HomeR
+
+postSetMessageR :: Handler ()
+postSetMessageR = do
+    msg <- runInputPost $ ireq textField "message"
+    setMessage $ toHtml msg
     redirect HomeR
 
 main :: IO ()
