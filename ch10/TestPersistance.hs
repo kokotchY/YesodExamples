e1d0a19848e53e34958f608616f18922b98112d6
Sébastien Canart <sebastien.canart@onprvp.fgov.be>
2014-09-25 09:30:22 +0200

    First persistence

---
+++ ch10/TestPersistance.hs
@@ -0,0 +1,37 @@
+{-# LANGUAGE OverloadedStrings, TypeFamilies, QuasiQuotes, TemplateHaskell #-}
+{-# LANGUAGE GADTs, FlexibleContexts #-}
+{-# LANGUAGE EmptyDataDecls, GeneralizedNewtypeDeriving #-}
+import Database.Persist
+import Database.Persist.Sqlite
+import Database.Persist.TH
+import Control.Monad.IO.Class (liftIO)
+
+share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
+Person
+    name String
+    age Int Maybe
+    deriving Show
+BlogPost
+    title String
+    authorId PersonId
+    deriving Show
+|]
+
+main :: IO ()
+main = runSqlite ":memory:" $ do
+    runMigration migrateAll
+
+    johnId <- insert $ Person "John Doe" $ Just 35
+    janeId <- insert $ Person "Jane Doe" Nothing
+
+    insert $ BlogPost "My first post" johnId
+    insert $ BlogPost "One more for good measure" johnId
+
+    oneJohnPost <- selectList [BlogPostAuthorId ==. johnId] [LimitTo 1]
+    liftIO $ print (oneJohnPost :: [Entity BlogPost])
+
+    john <- get johnId
+    liftIO $ print (john :: Maybe Person)
+
+    delete janeId
+    deleteWhere [BlogPostAuthorId ==. johnId]
