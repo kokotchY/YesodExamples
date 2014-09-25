6f09bd02956c0674019b66e2fb49917b043ceeb1
Sébastien Canart <sebastien.canart@onprvp.fgov.be>
2014-09-25 10:56:39 +0200

    First example of query

---
+++ ch10/Query.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE QuasiQuotes, TypeFamilies, GeneralizedNewtypeDeriving, TemplateHaskell, OverloadedStrings, GADTs, FlexibleContexts #-}
+import Database.Persist
+import Database.Persist.TH
+import Database.Persist.Sqlite
+import Data.Time
+import Control.Monad.IO.Class (liftIO)
+
+share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
+Person
+    firstName String
+    lastName String
+    age Int
+    PersonName firstName lastName
+    deriving Show
+|]
+
+main :: IO ()
+main = runSqlite ":memory:" $ do
+    runMigration migrateAll
+    personId <- insert $ Person "Michael" "Snoyman" 26
+    maybePerson <- get personId
+    case maybePerson of
+        Nothing -> liftIO $ putStrLn "Just kidding, not really there"
+        Just person -> liftIO $ print person
