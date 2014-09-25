2835cce85c0a86526c8906f49300b2f550e74b4e
Sébastien Canart <sebastien.canart@onprvp.fgov.be>
2014-09-25 18:52:20 +0200

    Add another resource

--- ch12/jsonConveniences.hs
+++ ch12/jsonConveniences.hs
@@ -17,12 +17,22 @@ data App = App
 
 mkYesod "App" [parseRoutes|
 / HomeR GET
+/second Home2R GET
 |]
 
 instance Yesod App
 
 getHomeR :: Handler Value
 getHomeR = returnJson $ Person "Bla" 21
+
+getHome2R :: Handler TypedContent
+getHome2R = selectRep $ do
+    provideRep $ return [shamlet|
+    <p>Hello, my name is #{name} and I am #{age} years old.
+    |]
+    provideJson person
+    where
+        person@Person {..} = Person "Bla" 21
 
 main :: IO ()
 main = warp 3000 App
