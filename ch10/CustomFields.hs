f115d6b6e682823240fef6e92aa499df03cc782a
Sébastien Canart <sebastien.canart@onprvp.fgov.be>
2014-09-25 14:15:09 +0200

    Example of CustomField

---
+++ ch10/CustomFields.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE QuasiQuotes, TypeFamilies, GeneralizedNewtypeDeriving, TemplateHaskell, OverloadedStrings, GADTs, FlexibleContexts #-}
+import Database.Persist.Sqlite
+import Database.Persist.TH
+import Employment
+
+share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
+Person
+    name String
+    employment Employment
+|]
+
+main :: IO ()
+main = runSqlite ":memory:" $ do
+    runMigration migrateAll
+
+    insert $ Person "Bruce Wayne" Retired
+    insert $ Person "Peter Parker" Unemployed
+    insert $ Person "Michael" Employed
+
+    return ()
