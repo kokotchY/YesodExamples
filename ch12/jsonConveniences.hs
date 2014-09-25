bfd62503f22015a8c531e226415eefd2612df561
Sébastien Canart <sebastien.canart@onprvp.fgov.be>
2014-09-25 20:04:06 +0200

    Everything together

--- ch12/jsonConveniences.hs
+++ ch12/jsonConveniences.hs
@@ -21,6 +21,7 @@ mkYesod "App" [parseRoutes|
 /example/2 Home2R GET
 /example/3 Home3R GET
 /example/4 Home4R GET
+/example/5 Home5R GET
 |]
 
 instance Yesod App
@@ -28,7 +29,7 @@ instance Yesod App
 mimeType :: ContentType
 mimeType = "text/haskell-show"
 
-pages = [HomeR, Home2R, Home3R, Home4R]
+pages = [HomeR, Home2R, Home3R, Home4R, Home5R]
 
 getRootR :: Handler Html
 getRootR = do
@@ -45,6 +46,8 @@ instance ToContent HaskellShow where
     toContent (HaskellShow x) = toContent $ show x
 instance ToTypedContent HaskellShow where
     toTypedContent = TypedContent mimeType . toContent
+instance HasContentType HaskellShow where
+    getContentType _ = mimeType
 
 getHomeR :: Handler Value
 getHomeR = returnJson $ Person "Bla" 21
@@ -68,6 +71,13 @@ getHome3R =
 getHome4R :: Handler HaskellShow
 getHome4R =
     return $ HaskellShow person
+    where
+        person = Person "Michael" 28
+
+getHome5R :: Handler TypedContent
+getHome5R = selectRep $ do
+    provideRep $ return $ HaskellShow person
+    provideJson person
     where
         person = Person "Michael" 28
 
