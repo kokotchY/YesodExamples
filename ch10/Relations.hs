004e9566f2638d50decdce89ef14f002306f017d
Sébastien Canart <sebastien.canart@onprvp.fgov.be>
2014-09-25 14:02:21 +0200

    Many to many association

--- ch10/Relations.hs
+++ ch10/Relations.hs
@@ -9,6 +9,12 @@ share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
 Person
     name String
     deriving Show
+Store
+    name String
+PersonStore
+    personId PersonId
+    storeId StoreId
+    UniquePersonStore personId storeId
 Car
     ownerId PersonId
     name String
@@ -19,7 +25,24 @@ main :: IO ()
 main = runSqlite ":memory:" $ do
     runMigration migrateAll
     bruce <- insert $ Person "Bruce Wayne"
+    michael <- insert $ Person "Michael"
+    target <- insert $ Store "Target"
+    gucci <- insert $ Store "Gucci"
+    sevenEleven <- insert $ Store "7-11"
     insert $ Car bruce "Bat Mobile"
     insert $ Car bruce "Porsche"
+
+    insert $ PersonStore bruce gucci
+    insert $ PersonStore bruce sevenEleven
+
+    insert $ PersonStore michael target
+    insert $ PersonStore michael sevenEleven
+
     cars <- selectList [CarOwnerId ==. bruce] []
-    liftIO $ mapM_ ((\x -> putStrLn $ "\t-" ++ show x) . entityVal) cars
+    liftIO $ displayList cars
+
+    store <- selectList [PersonId ==. bruce] []
+    liftIO $ displayList store
+
+displayList :: (Show a, PersistEntity a) => [Entity a] -> IO ()
+displayList = mapM_ ((\x -> putStrLn $ "\t- " ++ show x) . entityVal)
