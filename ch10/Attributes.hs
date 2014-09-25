92d17af7406519248563bdd8f546408177a75bec
Sébastien Canart <sebastien.canart@onprvp.fgov.be>
2014-09-25 12:04:34 +0200

    Another kind of default: with space

--- ch10/Attributes.hs
+++ ch10/Attributes.hs
@@ -10,20 +10,22 @@ Person
     name String
     age Int Maybe
     created UTCTime default=CURRENT_TIME
+    language String default='Haskell'
+    country String "default='El Salvador'"
     deriving Show
 |]
 
-peoples time = [
-    Person "bla" (Just 26) time,
-    Person "blo" Nothing time,
-    Person "ha" (Just 43) time,
-    Person "hihi" (Just 50) time,
-    Person "Adam" (Just 34) time]
+peoples time language country = [
+    Person "bla" (Just 26) time language country,
+    Person "blo" Nothing time language country,
+    Person "ha" (Just 43) time language country,
+    Person "hihi" (Just 50) time language country,
+    Person "Adam" (Just 34) time language country]
 
 main :: IO ()
 main = runSqlite ":memory:" $ do
     time <- liftIO getCurrentTime
     runMigration migrateAll
-    mapM_ insert $ peoples time
+    mapM_ insert $ peoples time "Haskell" "Belgium"
     people <- selectList ([] :: [Filter Person]) []
     liftIO $ mapM_ (print . entityVal) people
