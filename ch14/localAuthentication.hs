59b53f524218e7a7a38dac5cd4e89fd7ed388c18
Sébastien Canart <sebastien.canart@onprvp.fgov.be>
2014-10-02 12:46:36 +0200

    Local authentication that doesn't work yet because it use a sendmail

---
+++ ch14/localAuthentication.hs
@@ -0,0 +1,143 @@
+{-# LANGUAGE DeriveDataTypeable, FlexibleContexts, GADTs, GeneralizedNewtypeDeriving, MultiParamTypeClasses, OverloadedStrings, QuasiQuotes, TemplateHaskell, TypeFamilies #-}
+import Control.Monad (join)
+import Control.Monad.Logger (runNoLoggingT)
+import Data.Maybe (isJust)
+import Data.Text
+import qualified Data.Text.Lazy.Encoding
+import Data.Typeable (Typeable)
+import Database.Persist.Sqlite
+import Database.Persist.TH
+import Network.Mail.Mime
+import Text.Blaze.Html.Renderer.Utf8 (renderHtml)
+import Text.Hamlet (shamlet)
+import Text.Shakespeare.Text (stext)
+import Yesod
+import Yesod.Auth
+import Yesod.Auth.Email
+
+share [mkPersist sqlSettings { mpsGeneric = False }, mkMigrate "migrateAll"] [persistLowerCase|
+User
+    email Text
+    password Text Maybe
+    verkey Text Maybe
+    verified Bool
+    UniqueUser email
+    deriving Typeable
+|]
+
+data App = App SqlBackend
+
+mkYesod "App" [parseRoutes|
+/ HomeR GET
+/auth AuthR Auth getAuth
+|]
+
+instance Yesod App where
+    approot = ApprootStatic "http://localhost:3000"
+
+instance RenderMessage App FormMessage where
+    renderMessage _ _ = defaultFormMessage
+
+instance YesodPersist App where
+    type YesodPersistBackend App = SqlBackend
+    runDB f = do
+        App conn <- getYesod
+        runSqlConn f conn
+
+instance YesodAuth App where
+    type AuthId App = UserId
+
+    loginDest _ = HomeR
+    logoutDest _ = HomeR
+    authPlugins _ = [authEmail]
+
+    getAuthId creds = runDB $ do
+        x <- insertBy $ User (credsIdent creds) Nothing Nothing False
+        return $ Just $
+            case x of
+                Left (Entity userid _) -> userid
+                Right userid -> userid
+    authHttpManager = error "Email doesn't need an HTTP manager"
+
+instance YesodAuthEmail App where
+    type AuthEmailId App = UserId
+
+    afterPasswordRoute _ = HomeR
+
+    addUnverified email verkey =
+        runDB $ insert $ User email Nothing (Just verkey) False
+
+    sendVerifyEmail email _ verurl =
+        liftIO $ renderSendMail (emptyMail $ Address Nothing "noreply") {
+                mailTo = [Address Nothing email],
+                mailHeaders = [ ("Subject", "Verify your email address") ],
+                mailParts = [[textPart, htmlPart]]
+        }
+        where
+            textPart = Part {
+                partType = "text/plain; charset=utf-8",
+                partEncoding = None,
+                partFilename = Nothing,
+                partContent = Data.Text.Lazy.Encoding.encodeUtf8 [stext|
+                    Please confirm your email address by clicking on the link below.
+
+#{verurl}
+
+                    Thank you
+                    |],
+                partHeaders = []
+            }
+            htmlPart = Part {
+                partType = "text/html; charset=utf-8",
+                partEncoding = None,
+                partFilename = Nothing,
+                partContent = renderHtml [shamlet|
+                    <p>Please confirm your email address by clicking on the link below.
+                    <p>
+                        <a href=#{verurl}>#{verurl}
+                    <p>Thank you
+                    |],
+                partHeaders = []
+            }
+
+    getVerifyKey = runDB . fmap (join . fmap userVerkey) . get
+    setVerifyKey uid key = runDB $ update uid [UserVerkey =. Just key]
+    verifyAccount uid = runDB $ do
+        mu <- get uid
+        case mu of
+            Nothing -> return Nothing
+            Just u -> do
+                update uid [UserVerified =. True]
+                return $ Just uid
+    getPassword = runDB . fmap (join . fmap userPassword) . get
+    setPassword uid pass = runDB $ update uid [UserPassword =. Just pass]
+    getEmailCreds email = runDB $ do
+        mu <- getBy $ UniqueUser email
+        case mu of
+            Nothing -> return Nothing
+            Just (Entity uid u) -> return $ Just EmailCreds {
+                emailCredsId = uid,
+                emailCredsAuthId = Just uid,
+                emailCredsStatus = isJust $ userPassword u,
+                emailCredsVerkey =  userVerkey u,
+                emailCredsEmail = email
+            }
+    getEmail = runDB . fmap (fmap userEmail) . get
+
+getHomeR :: Handler Html
+getHomeR = do
+    maid <- maybeAuthId
+    defaultLayout [whamlet|
+        <p>Your current auth ID: #{show maid}
+        $maybe _ <- maid
+            <p>
+                <a href=@{AuthR LogoutR}>Logout
+        $nothing
+            <p>
+                <a href=@{AuthR LoginR}>Go to the login page
+    |]
+
+main :: IO ()
+main = runNoLoggingT $ withSqliteConn "email.db3" $ \conn -> liftIO $ do
+    runSqlConn (runMigration migrateAll) conn
+    warp 3000 $ App conn
