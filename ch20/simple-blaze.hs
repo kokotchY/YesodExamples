68a07453e2c89a75fc25e272dc62d6e6e8e118c2
Sébastien Canart <sebastien.canart@onprvp.fgov.be>
2014-10-11 09:37:57 +0200

    Simple example with blaze

---
+++ ch20/simple-blaze.hs
@@ -0,0 +1,14 @@
+{-# LANGUAGE OverloadedStrings #-}
+import Blaze.ByteString.Builder (Builder, fromByteString)
+import Network.HTTP.Types (status200)
+import Network.Wai (Application, responseBuilder)
+import Network.Wai.Handler.Warp (run)
+
+main :: IO ()
+main = run 3000 app
+
+app :: Application
+app _req sendResponse = sendResponse $ responseBuilder
+    status200
+    [("Content-Type", "text/plain")]
+    (fromByteString "Hello from blaze-builder!" :: Builder)
